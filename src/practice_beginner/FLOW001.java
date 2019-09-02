af4a25e6e3453d0192acf78b7f4b5d66dfb718b2
ravi <ravithesun02@gmail.com>
2019-09-02 18:17:06 +0530

    Initial

---
+++ src/practice_beginner/FLOW001.java
@@ -0,0 +1,48 @@
+package practice_beginner;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.Scanner;
+import java.util.StringTokenizer;
+
+public class FLOW001 {
+    public static void main(String[] args)
+    {
+        InputStream inputStream=System.in;
+        InputReader in=new InputReader(inputStream);
+        int t=in.nextInt();
+        while (t-->0)
+        {
+            int a=in.nextInt();
+            int b=in.nextInt();
+            System.out.println(a+b);
+        }
+
+    }
+    static class InputReader {
+        public BufferedReader reader;
+        public StringTokenizer tokenizer;
+
+        public InputReader(InputStream stream) {
+            reader = new BufferedReader(new InputStreamReader(stream), 32768);
+            tokenizer = null;
+        }
+
+        public String next() {
+            while (tokenizer == null || !tokenizer.hasMoreTokens()) {
+                try {
+                    tokenizer = new StringTokenizer(reader.readLine());
+                } catch (IOException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+            return tokenizer.nextToken();
+        }
+
+        public int nextInt() {
+            return Integer.parseInt(next());
+        }
+    }
+}
